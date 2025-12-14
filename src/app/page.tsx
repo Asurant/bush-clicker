e61e6262ec8f82de51fd36b0da956d007765b030
Asurant <149097598+Asurant@users.noreply.github.com>
2025-12-13 22:29:10 -0500

    Added Header and set up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <div className="">
       <main className="">
-        <h1 className={"text-6xl self-center my-10"}></h1>
-        <BushClick onClick={() => {console.log("boop!")}}/>
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className={"text-6xl self-center my-10"}>Haxmas Day 1</h1>
+          <BushClick onClick={() => {console.log("boop!")}}/>
+        </div>
       </main>
     </div>
   );
